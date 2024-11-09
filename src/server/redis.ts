48db4ef02b731228dd95daaf7265f9fab8acb512
Lucas Pinheiro <lucaspinheiroifsul@gmail.com>
2024-11-09 11:35:36 -0300

    fix redis

--- src/server/redis.ts
+++ src/server/redis.ts
@@ -1,7 +1,7 @@
 import Redis from "ioredis";
 
 export const redisConnection = new Redis(
-  process.env.REDIS_URL ?? "" + "?family=0",
+  process.env.REDIS_URL + "?family=0",
   {
     maxRetriesPerRequest: null,
     enableReadyCheck: false,
